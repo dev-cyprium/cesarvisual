18a047b93103341cb2ed3854fcefa902e6736dfa
Stefan Kupresak <stefan_vg@hotmail.com>
2018-12-10 00:53:04 +0100

    initialize git

---
+++ objects.js
@@ -0,0 +1,67 @@
+class Color {
+  constructor(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+}
+
+Color.GREEN = new Color(130, 199, 165);
+Color.DARK  = new Color(27, 33, 44);
+Color.BLUE  = new Color(1,69,172);
+
+class TextSquare {
+  constructor(x, y, text, color = new Color(130, 199, 165)) {
+    this.text = text;
+    this.size = TextSquare.SIZE;
+    this.textSize = 20;
+    this.x = x;
+    this.y = y;
+    this.offsetY = 7;
+    this.color = color;
+    this.easing = 0.05;
+    this.targetX = null;
+    this.maxSpeed = 11; // MUST be ODD!!
+    this.onFinishMove = null;
+    this.origin = x;
+  }
+
+  draw(p) {
+    p.fill(this.color.r, this.color.g, this.color.b);
+
+    p.noStroke();
+    p.rect(this.x, this.y, this.size, this.size, 7);
+    
+    p.fill(255);
+    p.textSize(this.textSize);
+    p.text(this.text, 
+      this.x + this.size / 2 - p.textWidth(this.text) / 2, 
+      this.y + this.size / 2 + this.offsetY);
+  }
+
+  setRelativeTarget(offset) {
+    this.targetX = this.origin - offset;
+  }
+
+  setTarget(tx) {
+    this.targetX = tx;
+  }
+
+  move(p) {
+    if(this.targetX) {
+      var toTarget = this.targetX - this.x;
+      var length = Math.abs(this.targetX - this.x);
+      if(length < 1) {
+        this.targetX = null;
+        if(this.onFinishMove) {
+          this.onFinishMove();
+        }
+        return;
+      }
+      toTarget = toTarget / length;
+      this.x += toTarget * this.maxSpeed;
+    }
+  }
+}
+
+TextSquare.SIZE = 50;
